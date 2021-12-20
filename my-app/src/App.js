9210dbf609180e8350966e92349aa8e451074fb8
madiyar0110 <seisembaevm@mail.ru>
2021-12-21 05:36:07 +0600

    commit
    Signed-off-by: madiyar0110 <seisembaevm@mail.ru>

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -175,7 +175,197 @@ class App extends Component {
         dataIndex: 'courseId',
       },
     ];
+    const columnsCoursesFull = [
+      {
+        title: "ID",
+        dataIndex: "id",
+      },
+      {
+        title: "Description",
+        dataIndex: "description",
+      },
+      {
+        title: "Price",
+        dataIndex: "price",
+      },
+      {
+        title: "Title",
+        dataIndex: "title",
+      },
+      {
+        title: "Rating",
+        dataIndex: "rating",
+      },
+    ];
+    const columnsCourses = [
+      {
+        title: "ID",
+        dataIndex: "id",
+      },
+      {
+        title: "Description",
+        dataIndex: "description",
+      },
+      {
+        title: "Price",
+        dataIndex: "price",
+      },
+      {
+        title: "Title",
+        dataIndex: "title",
+      },
+      {
+        title: "Rating",
+        dataIndex: "rating",
+      },
+      {
+        title: 'operation',
+        dataIndex: 'operation',
+        render: (_, record) =>
+          this.state.courses.length >= 1 ? (
+            <div>
+              <Popconfirm title="Sure to show?" onConfirm={() => this.setBooks(record)}>
+                <a>Show</a>
+              </Popconfirm>
+              <Popconfirm title="Sure to delete?" onConfirm={() => this.deleteCourseFromUser(record.id)}>
+                <a>Delete</a>
+              </Popconfirm>
+            </div>
 
+          ) : null,
+      },
+    ];
+
+    const textStyle = {
+      textAlignVertical: "center",
+      textAlign: "center",
+    };
+
+    return (
+      <div className="App">
+
+        <Row>
+
+          <Col>
+            <Table
+              onRow={(record, rowIndex) => {
+                return {
+                  onClick: (event) => { }, // click row
+                  onDoubleClick: (event) => { }, // double click row
+                  onContextMenu: (event) => { }, // right button click row
+                  onMouseEnter: (event) => { }, // mouse enter row
+                  onMouseLeave: (event) => { }, // mouse leave row
+                };
+              }}
+              style={{ marginLeft: 10 }}
+              columns={columnUsers}
+              dataSource={this.state.users}
+              bordered
+              title={() => "Users"}
+            />
+          </Col>
+          <Col style={{ marginLeft: 30 }}>
+            <Search
+              // style = {{ width : 150, height : 150, marginLeft : 370 }}
+              placeholder="input search text"
+              enterButton="Search"
+              size="large"
+              onSearch={(value) => this.getByUsername(value)}
+            />
+            <Table
+              columns={columnsCourses}
+              dataSource={this.state.courses}
+              bordered
+              title={() => "Courses"}
+            />
+          </Col>
+          <Col>
+            <Table
+              onRow={(record, rowIndex) => {
+                return {
+                  onClick: (event) => { }, // click row
+                  onDoubleClick: (event) => { }, // double click row
+                  onContextMenu: (event) => { }, // right button click row
+                  onMouseEnter: (event) => { }, // mouse enter row
+                  onMouseLeave: (event) => { }, // mouse leave row
+                };
+              }}
+              style={{ marginLeft: 10 }}
+              columns={columnsCoursesFull}
+              dataSource={this.state.fullCourses}
+              bordered
+              title={() => "All Courses"}
+            />
+          </Col>
+        </Row>
+        <hr></hr>
+
+
+
+        <Row>
+          <Col>
+            <h2>Add User</h2>
+            <Form
+              {...layout}
+              name="nest-messages"
+              onFinish={this.handleSubmitUser}
+            >
+
+              <Form.Item label="Username" name={"username"}>
+                <Input />
+              </Form.Item>
+              <Form.Item label="Name" name={"name"}>
+                <Input />
+              </Form.Item>
+              <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </Col>
+          <Col>
+            <h2>Add UserCourse</h2>
+            <Form
+              {...layout}
+              name="nest-messages"
+              onFinish={this.setUserCourse}
+            >
+
+              <Form.Item label="Course Id" name={"courseId"}>
+                <Input />
+              </Form.Item>
+              <Form.Item label="User ID" name={"userId"}>
+                <Input />
+              </Form.Item>
+              <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </Col>
+
+        </Row>
+        <Modal
+
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          okButtonProps={{ disabled: true }}
+          cancelButtonProps={{ disabled: true }}
+        >
+          <Table
+            columns={columnBooks}
+            dataSource={this.state.books}
+            bordered
+            title={() => "Books"}
+          />
+
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default App;
